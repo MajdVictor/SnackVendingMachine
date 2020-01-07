14c0a19c2b41e0395ea9420e5a7e962150a7f278
MajdVictor <majdhafiri@gmail.com>
2020-01-07 21:31:48 +0200

    added test_snack_coordinates unittest

--- machine_testing.py
+++ machine_testing.py
@@ -1,5 +1,5 @@
 import unittest
-from snacks_vending_machine import ValidateMoney
+from snacks_vending_machine import ValidateMoney,SnackSlot
 
 class MachineTesting(unittest.TestCase):
 
@@ -9,6 +9,21 @@ class MachineTesting(unittest.TestCase):
         result = validate_obj.return_amount()
         self.assertEqual(result, 15)
 
+    def test_snack_coordinates(self):
+        snack_slot = SnackSlot()
+
+        x,y = snack_slot.get_snack_coordinates(25)
+        self.assertEqual(x, 5)
+        self.assertEqual(y, 5)
+
+        result = snack_slot.get_snack_coordinates(0)
+        self.assertFalse(result)
+
+        result = snack_slot.get_snack_coordinates(26)
+        self.assertFalse(result)
+    
+    def test_check_snack_availability(self):
+        
 if __name__ == "__main__":
     unittest.main()
     
